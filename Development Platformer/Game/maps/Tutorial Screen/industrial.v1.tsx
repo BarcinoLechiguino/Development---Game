2904e0baf09a4ae12856832bda0dfb1582742bbb
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2019-10-10 11:11:42 +0200

    1st Screen Implemented

--- Development Platformer/Game/maps/Tutorial Screen/industrial.v1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="industrial.v1" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="industrial.v1.png" width="1024" height="1024"/>
-</tileset>
