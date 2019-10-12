c4673401a5042cfa03a34e8f78cd90670bd60513
BarcinoLechuguino <angotov@gmail.com>
2019-10-12 17:19:23 +0200

    Input fixed (name.create() passed the wrong string)

--- Development Platformer/Motor2D/j1Player1.cpp
+++ Development Platformer/Motor2D/j1Player1.cpp
@@ -10,10 +10,11 @@
 
 j1Player1::j1Player1() //Constructor. Called at the first frame.
 {
-	
+	//String that will be given to the different functions (Awake(), Load()...) to generate the handler node.
+	name.create("player"); //The string has to be the same as the name of the node in the xml file.
 };
 
-j1Player1::~j1Player1()  //Destructor. Called at the first frame.
+j1Player1::~j1Player1()  //Destructor. Called at the last frame.
 {
 
 };
@@ -23,16 +24,210 @@ bool j1Player1::Init()
 	return true;
 };
 
-bool j1Player1::Awake(int x, int y) 
+bool j1Player1::Awake(pugi::xml_node& config) 
 {	
-
-	/*p1.speed_x = config.child("player_1").child("speed_x").attribute("value").as_float();
+	p1.speed_x = config.child("speed_x").attribute("value").as_float();
 	p1.speed_y = config.child("speed_y").attribute("value").as_float();
-	p1.max_speed = config.child("player_1").child("max_speed").attribute("value").as_float();
-	p1.acceleration = config.child("player_1").child("acceleration").attribute("value").as_float();
-	p1.p1_gravity = config.child("gravity").attribute("value").as_float();*/
+	p1.max_speed = config.child("max_speed").attribute("value").as_float();
+	
+	p1.acceleration = config.child("acceleration").attribute("value").as_float();
+	p1.p1_gravity = config.child("gravity").attribute("value").as_float();
 
-	pugi::xml_parse_result result = file.load_file("Config.xml");
+	return true;
+};
+
+bool j1Player1::Start() 
+{
+	p1.p1_position = { 300.0f, p1.floor };
+	p1.p1_HitBox = { (int)p1.p1_position.x,(int)p1.p1_position.y, p1.sprite_width, p1.sprite_height }; //Casked to int "(int)" for optimization.
+
+	return true;
+};
+
+bool j1Player1::PreUpdate() 
+{
+	//p1.p1_time = SDL_GetTicks() / 20;
+	
+	p1.p1_State = idle_P1;
+
+	if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) 
+	{
+		p1.p1_State = goingRight_P1;
+	}
+
+	if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) 
+	{
+		p1.p1_State = goingLeft_P1;
+	}
+
+	if (App->input->GetKey(SDL_SCANCODE_W) == KEY_DOWN) 
+	{
+		//p1.p1_time_at_jump = p1.p1_time;
+		p1.p1_State = jumping_P1;
+	}
+
+	return true;
+};
+
+bool j1Player1::Update(float dt) 
+{
+	p1_frames++;
+
+	switch (p1.p1_State)
+	{
+
+	case idle_P1:
+
+		p1.speed_x = 0;
+
+		break;
+
+	case goingRight_P1:
+
+		//p1.p1_position.x += 10;
+
+		LOG("GOING RIGHT %d %d", p1.speed_x, p1.max_speed);
+
+		while (p1.speed_x != p1.max_speed)
+		{
+			p1.speed_x += p1.acceleration;
+
+			if (p1.speed_x > p1.max_speed)
+			{
+				p1.speed_x = p1.max_speed;
+			}
+		}
+
+		p1.p1_position.x += p1.speed_x; //p1.speed_x is positive here.
+
+		break;
+
+	case goingLeft_P1:
+
+		while (p1.speed_x != -p1.max_speed)
+		{
+			p1.speed_x -= p1.acceleration;
+
+			if (p1.speed_x < -p1.max_speed)
+			{
+				p1.speed_x = -p1.max_speed;
+			}
+		}
+
+		p1.p1_position.x += p1.speed_x;  //p1.speed_x  is negative here.
+
+		break;
+
+	case jumping_P1:
+		//p1.p1_jump_time = p1.p1_time - p1.p1_time_at_jump;
+		//p1.p1_position.y = p1.floor - (10*p1.p1_jump_time) + (0.5*(p1.p1_gravity) * (p1.p1_jump_time*p1.p1_jump_time));
+		//p1.p1_position.x = p1.speed_x;
+		//position_P1.y += p1.speed_y;
+
+		break;
+	}
+
+	p1.p1_HitBox.x = p1.p1_position.x;
+	//p1.p1_HitBox.y = p1.p1_position.y;
+
+	if (p1.p1_position.y <= p1.floor)
+	{
+		p1.p1_position.y = p1.floor;
+	}
+
+	App->render->DrawQuad(p1.p1_HitBox, 255, 0, 0);
+
+	return true;
+};
+
+bool j1Player1::PostUpdate() 
+{
+	return true;
+};
+
+bool j1Player1::cleanUp() 
+{
+	return true;
+};
+
+void j1Player1::Draw(SDL_Texture* sprites, float dt) 
+{
+
+}
+
+/*
+	p1_frames++;
+
+	switch (p1.p1_State)
+	{
+
+	case idle_P1:
+
+		p1.speed_x = 0;
+
+		break;
+
+	case goingRight_P1:
+
+		//p1.p1_position.x += 10;
+
+
+		while (p1.speed_x != p1.max_speed)
+		{
+			p1.speed_x += p1.acceleration;
+
+			if (p1.speed_x > p1.max_speed)
+			{
+				p1.speed_x = p1.max_speed;
+			}
+		}
+
+		p1.p1_position.x += p1.speed_x; //p1.speed_x is positive here.
+
+		break;
+
+	case goingLeft_P1:
+		p1.p1_position.x -= 10;
+
+		while (p1.speed_x != -p1.max_speed)
+		{
+			p1.speed_x -= p1.acceleration;
+
+			if (p1.speed_x < -p1.max_speed)
+			{
+				p1.speed_x = -p1.max_speed;
+			}
+		}
+
+		p1.p1_position.x += p1.speed_x;  //p1.speed_x  is negative here.
+
+		break;
+
+	case jumping_P1:
+		//p1.p1_jump_time = p1.p1_time - p1.p1_time_at_jump;
+		//p1.p1_position.y = p1.floor - (10*p1.p1_jump_time) + (0.5*(p1.p1_gravity) * (p1.p1_jump_time*p1.p1_jump_time));
+		//p1.p1_position.x = p1.speed_x;
+		//position_P1.y += p1.speed_y;
+
+		break;
+	}
+
+	p1.p1_HitBox.x = p1.p1_position.x;
+	//p1.p1_HitBox.y = p1.p1_position.y;
+
+	if (p1.p1_position.y <= p1.floor)
+	{
+		p1.p1_position.y = p1.floor;
+	}
+
+	App->render->DrawQuad(p1.p1_HitBox, 255, 0, 0);
+
+	return true;
+*/
+
+/*
+Awake(int x, int y) code Gerard:
+pugi::xml_parse_result result = file.load_file("Config.xml");
 
 	if (result != NULL)
 	{
@@ -80,112 +275,10 @@ bool j1Player1::Awake(int x, int y)
 
 		top_jump = true;
 	}
+*/
 
-	return true;
-};
-
-bool j1Player1::Start() 
-{
-	//p1.p1_position = { 250, p1.floor };
-	//p1.p1_HitBox = { (int)p1.p1_position.x,(int)p1.p1_position.y, p1.sprite_width, p1.sprite_height }; //Casked to int "(int)" for optimization.
-
-	return true;
-};
-
-bool j1Player1::PreUpdate() 
-{
-	//p1.p1_time = SDL_GetTicks() / 20;
-	//
-	//p1.p1_State = idle_P1;
-
-	//if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) 
-	//{
-	//	p1.p1_State = goingRight_P1;
-	//}
-
-	//if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) 
-	//{
-	//	p1.p1_State = goingLeft_P1;
-	//}
-
-	//if (App->input->GetKey(SDL_SCANCODE_W) == KEY_DOWN) 
-	//{
-	//	//p1.p1_time_at_jump = p1.p1_time;
-	//	p1.p1_State = jumping_P1;
-	//}
-
-	return true;
-};
-
-bool j1Player1::Update(float dt) 
-{
-	//p1_frames++;
-
-	//switch (p1.p1_State) 
-	//{
-
-	//case idle_P1:
-	//	
-	//	p1.speed_x = 0;
-
-	//	break;
-
-	//case goingRight_P1:
-
-	//	//p1.p1_position.x += 10;
-	//
-
-	//	while (p1.speed_x != p1.max_speed)
-	//	{
-	//		p1.speed_x += p1.acceleration;
-
-	//		if (p1.speed_x > p1.max_speed)
-	//		{
-	//			p1.speed_x = p1.max_speed;
-	//		}
-	//	}
-	//	
-	//	p1.p1_position.x += p1.speed_x; //p1.speed_x is positive here.
-
-	//	break;
-
-	//case goingLeft_P1:
-	//	p1.p1_position.x -= 10;
-	//	
-	//	while (p1.speed_x != -p1.max_speed)
-	//	{
-	//		p1.speed_x -= p1.acceleration;
-
-	//		if (p1.speed_x < -p1.max_speed)
-	//		{
-	//			p1.speed_x = -p1.max_speed;
-	//		}
-	//	}
-
-	//	p1.p1_position.x += p1.speed_x;  //p1.speed_x  is negative here.
-
-	//	break;
-
-	//case jumping_P1:
-	//	//p1.p1_jump_time = p1.p1_time - p1.p1_time_at_jump;
-	//	//p1.p1_position.y = p1.floor - (10*p1.p1_jump_time) + (0.5*(p1.p1_gravity) * (p1.p1_jump_time*p1.p1_jump_time));
-	//	//p1.p1_position.x = p1.speed_x;
-	//	//position_P1.y += p1.speed_y;
-
-	//	break;
-	//}
-
-	//p1.p1_HitBox.x = p1.p1_position.x;
-	////p1.p1_HitBox.y = p1.p1_position.y;
-
-	//if (p1.p1_position.y <= p1.floor)
-	//{
-	//	p1.p1_position.y = p1.floor;
-	//}
-	//
-	//App->render->DrawQuad(p1.p1_HitBox, 255, 0, 0);
-
-	animation = &idle;
+/*
+animation = &idle;
 
 	if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT && dt != 0)
 	{
@@ -230,26 +323,9 @@ bool j1Player1::Update(float dt)
 			animation = &jump_anim;
 			position.y -= (speed.y + gravity)*dt;
 		}
-		
+
 	}
 
 	position.y += gravity * dt;
 	stay_in_platform = false;
-
-	return true;
-};
-
-bool j1Player1::PostUpdate() 
-{
-	return true;
-};
-
-bool j1Player1::cleanUp() 
-{
-	return true;
-};
-
-void j1Player1::Draw(SDL_Texture* sprites, float dt) {
-
-
-}
+*/
--- Development Platformer/Motor2D/j1Player1.h
+++ Development Platformer/Motor2D/j1Player1.h
@@ -7,14 +7,14 @@
 #include "j1Input.h"
 #include "Animation.h"
 
-//enum P1_State 
-//{
-//	idle_P1,
-//	goingRight_P1,
-//	goingLeft_P1,
-//	jumping_P1,
-//};
-//
+enum P1_State 
+{
+	idle_P1,
+	goingRight_P1,
+	goingLeft_P1,
+	jumping_P1
+};
+
 //struct Input_P1
 //{
 //	bool A_active; //Go Left
@@ -23,29 +23,29 @@
 //	bool S_active; //Crouch
 //	bool C_active; //Boost
 //};
-//
-//struct Player1
-//{
-//	float speed_x; //Speed on X. Basic Movement
-//	float speed_y; //Speed on Y. Used for jumps.
-//	float max_speed; //Cruiser speed for the player.
-//	float acceleration; //Time it takes the player to reach Cruiser Speed.
-//
-//	float p1_gravity; //Acceleration variable for jumps. Gravitational Pull
-//
-//	int p1_time; //Time P1 has been running.
-//	int p1_time_at_jump; //Time when the jump button is pressed.
-//	int p1_jump_time; //jumptimeP1 = time_P1 - timeAtJump_P1
-//	
-//	int sprite_width = 20;
-//	int sprite_height = 30;
-//
-//	float floor = 500.0f;
-//
-//	SDL_Rect p1_HitBox;
-//	p2Point<float> p1_position;
-//	P1_State p1_State;
-//};
+
+struct Player1
+{
+	float speed_x; //Speed on X. Basic Movement
+	float speed_y; //Speed on Y. Used for jumps.
+	float max_speed; //Cruiser speed for the player.
+
+	float acceleration; //Time it takes the player to reach Cruiser Speed.
+	float p1_gravity; //Acceleration variable for jumps. Gravitational Pull
+
+	int p1_time; //Time P1 has been running.
+	int p1_time_at_jump; //Time when the jump button is pressed.
+	int p1_jump_time; //jumptimeP1 = time_P1 - timeAtJump_P1
+	
+	int sprite_width = 20;
+	int sprite_height = 30;
+
+	float floor = 500.0f;
+
+	SDL_Rect p1_HitBox; //Rectangle that represents the player.
+	p2Point<float> p1_position; //Vector with the position of P1
+	P1_State p1_State; //Adds the state enum to the player's variables.
+};
 
 struct SDL_Texture;
 
@@ -58,19 +58,29 @@ public://methods
 	virtual ~j1Player1();
 
 	bool Init();
-	bool Awake(int x, int y);
+
+	bool Awake(pugi::xml_node&);
+
 	bool Start();
+
 	bool PreUpdate();
+
 	bool Update(float dt);
+
 	bool PostUpdate();
+
 	bool cleanUp();
+
 	void Draw(SDL_Texture* sprites, float dt);
 
+	private:
 
-public://variables
+
+public: //P1 Variables
 	
+	Player1 p1;
 
-	Animation * animation = nullptr;
+	/*Animation * animation = nullptr;
 
 	int jump;
 	int distance;
@@ -79,21 +89,21 @@ public://variables
 	bool top_jump = true;
 	bool stay_in_platform = false;
 	iPoint speed;
-	iPoint position;
+	iPoint position;*/
 
 private:
 
-	Animation idle;
-	Animation jump_anim;
-	Animation run;
-	Animation crouch;
-	iPoint player_size;
+	//Animation idle;
+	//Animation jump_anim;
+	//Animation run;
+	//Animation crouch;
+	//iPoint player_size;
 
-	pugi::xml_document file;
+	//pugi::xml_document file;
 
-	float p1_frames;
+	float p1_frames = 0;
 	//bool runFrames = false;
-	float p1_startFrame;
+	float p1_startFrame = 0;
 };
 
 #endif __j1Player_1_H__
