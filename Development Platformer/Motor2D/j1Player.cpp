b4587101a7ab5d55a495327962043fe4ff362b97
BarcinoLechuguino <angotov@gmail.com>
2019-11-29 00:33:44 +0100

    Revert "LoadNextMap() now works as intended (the strings passed to the FadeToBlack() method were incorrect (Capital M on map instead of just m)"
    This reverts commit b0debbdefc1b714089753089041cf22ed58e771c.

--- Development Platformer/Motor2D/j1Player.cpp
+++ Development Platformer/Motor2D/j1Player.cpp
@@ -256,11 +256,15 @@ void j1Player::LoadNextMap()										//REVISE THIS HERE. Loads the map but coll
 {
 	if (App->scene->firstMap == false)
 	{
-		App->fadescene->FadeToBlack("Test_map.tmx");				//Loads the 1st level.
+		App->scene->firstMap = true;
+		App->scene->secondMap = false;
+		App->fadescene->FadeToBlack("Test_Map.tmx");				//Loads the 1st level.
 	}
 	if (App ->scene->secondMap == false)
 	{
-		App->fadescene->FadeToBlack("Test_map_2.tmx");
+		App->scene->secondMap = true;
+		App->scene->firstMap = false;
+		App->fadescene->FadeToBlack("Test_Map_2.tmx");
 	}	
 }
 
